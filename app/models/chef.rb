90c2687930b396747d042948b51ec27418c123c4
Bohemian Codes <chavelitaloca@gmail.com>
2017-10-25 19:46:04 +0200

    added validations to models via TDD

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,4 +1,8 @@
 class Chef < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :chefname, presence: true, length: {maximum: 30}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 100},
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false}
+
 end
---
+++ test/models/chef_test.rb
@@ -0,0 +1,56 @@
+require "test_helper"
+
+class ChefTest < ActiveSupport::TestCase
+
+  def setup
+    @chef = Chef.new(chefname: "Eliane", email: "test@test.de")
+  end
+
+  test "should be valid" do
+    assert @chef.valid?
+  end
+
+  test "chefname should be present" do
+    @chef.chefname = " "
+    assert_not @chef.valid?
+  end
+
+  test "chefname shouldbe less than 30 char" do
+    @chef.chefname = "x" * 40
+    assert_not @chef.valid?
+  end
+
+  test "email should be present" do
+    @chef.email = " "
+    assert_not @chef.valid?
+  end
+
+  test "email should not be longer than 255 char" do
+    @chef.email ="x" * 200
+    assert_not @chef.valid?
+  end
+
+  test "email should be valid format" do
+    valid_emails = %w[user@example.com MASHRUR@gmail.com m.first@yahoo.ca john+smith@co.uk.org eliane@gmail.berlin]
+    valid_emails.each do |valid|
+      @chef.email = valid
+      assert @chef.valid?, "#{valid.inspect} should be valid"
+    end
+  end
+
+  test "should reject invalid email addresses" do
+    invalid_emails =%w[mashrur@example MASHRUR@example,com jie@bar+foo.de]
+    invalid_emails.each do |invalid|
+      @chef.email = invalid
+      assert_not @chef.valid?, "#{invalid.inspect} should be invalid"
+    end
+  end
+
+  test "email should be unique and case insensitive" do
+    duplicatechef = @chef.dup
+    duplicatechef.email = @chef.email.upcase
+    @chef.save
+    assert_not duplicatechef.valid?
+  end
+
+end
