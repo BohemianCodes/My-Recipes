b67bf7dd92888c0ceae23378f0e0b296390199d2
Bohemian Codes <chavelitaloca@gmail.com>
2017-11-02 20:17:40 +0100

    adding partial for title, adding gravatar method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  #the place where you write methods for the views
+  def gravatar_for(user, options = {size: 80})
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "http://secure.gravatar.com/avatar#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "img-circle")
+  end
 end
